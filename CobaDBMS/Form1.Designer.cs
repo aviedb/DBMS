08d41b30701c888107da29a8a5cd37031ec0fddd
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-09-23 06:56:35 +0700

    Initial commit

---
+++ CobaDBMS/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace CobaDBMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listview_1061 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.firstNameTB_1061 = new System.Windows.Forms.TextBox();
+            this.lastNameTB_1061 = new System.Windows.Forms.TextBox();
+            this.addressBT_1061 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listview_1061
+            // 
+            this.listview_1061.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader5});
+            this.listview_1061.Location = new System.Drawing.Point(383, 12);
+            this.listview_1061.Name = "listview_1061";
+            this.listview_1061.Size = new System.Drawing.Size(859, 600);
+            this.listview_1061.TabIndex = 0;
+            this.listview_1061.UseCompatibleStateImageBehavior = false;
+            this.listview_1061.View = System.Windows.Forms.View.Details;
+            this.listview_1061.SelectedIndexChanged += new System.EventHandler(this.listView_1061_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            this.columnHeader1.Width = 68;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 190;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Name";
+            this.columnHeader3.Width = 142;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Address";
+            this.columnHeader5.Width = 226;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address";
+            // 
+            // firstNameTB_1061
+            // 
+            this.firstNameTB_1061.Location = new System.Drawing.Point(159, 38);
+            this.firstNameTB_1061.Name = "firstNameTB_1061";
+            this.firstNameTB_1061.Size = new System.Drawing.Size(188, 31);
+            this.firstNameTB_1061.TabIndex = 4;
+            // 
+            // lastNameTB_1061
+            // 
+            this.lastNameTB_1061.Location = new System.Drawing.Point(159, 75);
+            this.lastNameTB_1061.Name = "lastNameTB_1061";
+            this.lastNameTB_1061.Size = new System.Drawing.Size(188, 31);
+            this.lastNameTB_1061.TabIndex = 5;
+            // 
+            // addressBT_1061
+            // 
+            this.addressBT_1061.Location = new System.Drawing.Point(159, 112);
+            this.addressBT_1061.Name = "addressBT_1061";
+            this.addressBT_1061.Size = new System.Drawing.Size(188, 31);
+            this.addressBT_1061.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 48);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.updateBtn_1061_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 48);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.deleteBtn_1061_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(224, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 48);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Show";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.showBtn_1061_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 48);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.saveBtn_1061_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 624);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.addressBT_1061);
+            this.Controls.Add(this.lastNameTB_1061);
+            this.Controls.Add(this.firstNameTB_1061);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listview_1061);
+            this.Name = "Form1";
+            this.Text = "Form_1061";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listview_1061;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox firstNameTB_1061;
+        private System.Windows.Forms.TextBox lastNameTB_1061;
+        private System.Windows.Forms.TextBox addressBT_1061;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ CobaDBMS/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace CobaDBMS
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=db;SslMode=none;";
+        int currentId = 0;
+        int currentRow = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showBtn_1061_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM mahasiswa";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 600;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+                // Success, now list 
+
+                // If there are available rows
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // Example to save in the listView1 :
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+                        var listViewItem = new ListViewItem(row);
+                        listview_1061.Items.Add(listViewItem);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void saveBtn_1061_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO mahasiswa(`First Name`, `Last Name`, `Address`) VALUES ('" + 
+                firstNameTB_1061.Text + "', '" + lastNameTB_1061.Text + "', '" + addressBT_1061.Text + "')";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Student successfully added");
+
+                this.showBtn_1061_Click(sender, e);
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listView_1061_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListViewItem lvi = listview_1061.SelectedItems[0];
+
+            firstNameTB_1061.Text = lvi.SubItems[1].Text;
+            lastNameTB_1061.Text = lvi.SubItems[2].Text;
+            addressBT_1061.Text = lvi.SubItems[3].Text;
+            currentId = int.Parse(lvi.SubItems[0].Text);
+            currentRow = lvi.Index;
+        }
+
+        private void updateBtn_1061_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE mahasiswa SET `First Name` = '" + firstNameTB_1061.Text + 
+                "', `Last Name` = '" + lastNameTB_1061.Text + "', `Address` = '" + addressBT_1061.Text + 
+                "' WHERE `Id` = " + currentId + ";";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Student successfully updated");
+               
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void deleteBtn_1061_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM mahasiswa WHERE `Id` = " + currentId + ";";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Student successfully deleted");
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
